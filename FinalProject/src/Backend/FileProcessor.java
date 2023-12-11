c1d6959e2e8b546c3fb76f52868b033497e7f5e0
Lestat <2233915@slu.edu.ph>
2023-12-11 16:05:49 +0800

    +completed method that creates a graph from text input

--- FinalProject/src/Backend/FileProcessor.java
+++ FinalProject/src/Backend/FileProcessor.java
@@ -9,20 +9,47 @@ public class FileProcessor {
 
     public Graph fetchFromFile(String localPath){
         File graphFile = new File(localPath);
-        ArrayList<String[]> fetchedEdges = new ArrayList<String[]>();
+        Graph outputGraph = new Graph();
+
+        ArrayList<Edge> edgeList = new ArrayList<Edge>();
+        ArrayList<Vertex> vertexList = new ArrayList<Vertex>();
         try{
             Scanner fileRead = new Scanner(graphFile);
             while(fileRead.hasNext()){
-                fetchedEdges.add(fileRead.next().split(",(?![^(]*[A-Za-z],[A-Za-z]\\))"));
+                String[] fetchedEdges = fileRead.next().split(",");
+                int startingId = Integer.parseInt(fetchedEdges[0]);
+                Vertex startingVertex = new Vertex(startingId);
+                int endingId = Integer.parseInt(fetchedEdges[1]);
+                Vertex endingVertex = new Vertex(endingId);
+                double edgeWeight = Double.parseDouble(fetchedEdges[2]);
+                int edgeId = Integer.parseInt(fetchedEdges[3]);
+                Edge currentEdge = new Edge(startingVertex, endingVertex, edgeWeight,edgeId);
+                edgeList.add(currentEdge);
+                startingVertex.addEdge(currentEdge);
+                endingVertex.addEdge(currentEdge);
+                addToVertexList(vertexList, startingVertex, currentEdge);
+                addToVertexList(vertexList, endingVertex, currentEdge);
             }
+            for(int x = 0; x < vertexList.size(); x++){
+                outputGraph.addVertex(vertexList.get(x));
+            }
+
+            return outputGraph;
 
 
         }catch(FileNotFoundException fileNotFound){
             fileNotFound.printStackTrace();
         }
-
-
         return null;
+    }
+
+    private void addToVertexList(ArrayList<Vertex> vertexList, Vertex vertex, Edge edge){
+        if(!(vertexList.contains(vertex))){
+            vertexList.add(vertex);
+        }else{
+            Vertex v = vertexList.get(vertexList.indexOf(vertex));
+            v.addEdge(edge);
+        }
     }
 
 }
